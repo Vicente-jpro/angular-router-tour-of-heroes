ea52e9fac5eb34126f71aeacb78fc921c8685d6f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-10-12 15:41:40 +0100

    Crises-mock added

---
+++ src/app/crisis-list/crisis.ts
@@ -0,0 +1,4 @@
+export class Crisis{
+    id: number = 0
+    nome: string = ''
+}
---
+++ src/app/crisis-list/mock-crisis.ts
@@ -0,0 +1,8 @@
+import { Crisis } from "./crisis";
+
+export const CRISES: Crisis[] = [
+  { id: 1, nome: 'Dragon Burning Cities' },
+  { id: 2, nome: 'Sky Rains Great White Sharks' },
+  { id: 3, nome: 'Giant Asteroid Heading For Earth' },
+  { id: 4, nome: 'Procrastinators Meeting Delayed Again' },
+];
